9837d84fbfeef5ddabe9f1dd18d133a794717cdd
Piotr Łachut <lachut.piotr@outlook.com>
2023-01-26 20:35:06 +0100

    Add project files.

---
+++ CollaborationSingalR/Server/Hubs/CollaborateHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CollaborationSingalR.Server.Hubs
+{
+    public class CollaborateHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await SendEvent("", "User connected");
+            await base.OnConnectedAsync();
+        }
+
+        public async Task SendEvent(string user, string move)
+        {
+            await Clients.All.SendAsync("ReceiveMoveObject", user, move);
+        }
+    }    
+}
